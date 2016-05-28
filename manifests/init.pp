6324e7990a5a9e051f7d4d03467c6d2beb609ce1
Vadym Chepkov <vchepkov@gmail.com>
2016-05-28 08:03:03 -0400

    improved support for modern versions of puppet
    used logic from hunner-hiera module

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,17 +5,19 @@
 class puppet_hipchat (
   $api_key,
   $room,
-  $notify_color = 'red',
-  $notify_room  = false,
-  $statuses     = [ 'failed' ],
-  $config_file  = "${puppet_hipchat::params::puppetconf_path}/hipchat.yaml",
-  $package_name = $puppet_hipchat::params::package_name,
-  $provider     = $puppet_hipchat::params::provider,
-  $owner        = $puppet_hipchat::params::owner,
-  $group        = $puppet_hipchat::params::group,
-  $puppetboard  = $puppet_hipchat::params::puppetboard,
-  $dashboard    = $puppet_hipchat::params::dashboard,
-  $exclude      = [ 'NONE' ],
+  $enabled        = true,
+  $notify_color   = 'red',
+  $notify_room    = false,
+  $statuses       = ['failed'],
+  $config_file    = $puppet_hipchat::params::config_file,
+  $package_name   = $puppet_hipchat::params::package_name,
+  $provider       = $puppet_hipchat::params::provider,
+  $master_service = $puppet_hipchat::params::master_service,
+  $owner          = $puppet_hipchat::params::owner,
+  $group          = $puppet_hipchat::params::group,
+  $puppetboard    = $puppet_hipchat::params::puppetboard,
+  $dashboard      = $puppet_hipchat::params::dashboard,
+  $exclude        = ['NONE'],
 ) inherits puppet_hipchat::params {
 
   file { $config_file:
@@ -29,5 +31,23 @@ class puppet_hipchat (
   package { $package_name:
     ensure   => installed,
     provider => $provider,
+  }
+
+  Ini_subsetting {
+    path                 => $::settings::config,
+    section              => 'master',
+    setting              => 'reports',
+    subsetting_separator => ',',
+    subsetting           => 'hipchat',
+    value                => '',
+  }
+  if $enabled {
+    ini_subsetting { 'puppet_hipchat report': ensure => present, }
+  } else {
+    ini_subsetting { 'puppet_hipchat report': ensure => absent, }
+  }
+
+  Service <| title == $master_service |> {
+    subscribe +> Class[$title],
   }
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -7,16 +7,38 @@ class puppet_hipchat::params {
   $package_name = 'hipchat'
   $puppetboard  = 'NONE'
   $dashboard    = 'NONE'
+  $config_file  = "${::settings::confdir}/hipchat.yaml"
 
-  if $::is_pe or $::is_pe == 'true' {
-    $puppetconf_path = '/etc/puppetlabs/puppet'
-    $provider        = 'pe_gem'
-    $owner           = 'pe-puppet'
-    $group           = 'pe-puppet'
+  if $::is_pe or str2bool($::is_pe) {
+    $owner = 'pe-puppet'
+    $group = 'pe-puppet'
+
+    if $::pe_version and versioncmp($::pe_version, '3.7.0') >= 0 {
+      $provider       = 'pe_puppetserver_gem'
+      $master_service = 'pe-puppetserver'
+    } else {
+      $provider       = 'pe_gem'
+      $master_service = 'pe-httpd'
+    }
   } else {
-    $puppetconf_path = '/etc/puppet'
-    $provider        = 'gem'
-    $owner           = 'puppet'
-    $group           = 'puppet'
+    if $::puppetversion and versioncmp($::puppetversion, '4.0.0') >= 0 {
+      if $::pe_server_version {
+        $master_service = 'pe-puppetserver'
+        $provider       = 'puppetserver_gem'
+      } else {
+        $master_service = 'puppetserver'
+        $provider       = 'puppet_gem'
+      }
+    } else {
+      $master_service = 'puppetmaster'
+      $provider       = 'gem'
+    }
+    if $::pe_server_version {
+      $owner = 'pe-puppet'
+      $group = 'pe-puppet'
+    } else {
+      $owner = 'puppet'
+      $group = 'puppet'
+    }
   }
 }
